528c68282b1e207d4749a76dbac8ee79a6f132f0
Vijay Sridhar <sridvijay@gmail.com>
2016-01-23 19:56:13 -0500

    added graph view to split view controller

--- Logical/Logical/DetailViewController.swift
+++ Logical/Logical/DetailViewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import GraphView
 
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
+    var graphView: GraphView!
+    
+    let size = CGSize(width: 80, height: 80)
 
 
     var detailItem: AnyObject? {
@@ -31,6 +35,46 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let red: UIView = RoundedView()
+        red.backgroundColor = .redColor()
+        red.frame.size = size
+        
+        let blue: UIView = RoundedView()
+        blue.backgroundColor = .blueColor()
+        blue.frame.size = size
+        
+        let green: UIView = RoundedView()
+        green.backgroundColor = .greenColor()
+        green.frame.size = size
+        
+        let yellow: UIView = RoundedView()
+        yellow.backgroundColor = .yellowColor()
+        yellow.frame.size = size
+        
+        let purple: UIView = RoundedView()
+        purple.backgroundColor = .purpleColor()
+        purple.frame.size = size
+        
+        let brown: UIView = RoundedView()
+        brown.backgroundColor = .brownColor()
+        brown.frame.size = size
+        
+        let orange: UIView = RoundedView()
+        
+        orange.backgroundColor = .orangeColor()
+        orange.frame.size = size
+        
+        let gray: UIView = RoundedView()
+        gray.backgroundColor = .grayColor()
+        gray.frame.size = size
+        
+        self.graphView = GraphView(graph: Graph(nodes: [red, blue ,green ,yellow ,purple ,brown ,orange ,gray], edges: [(red,blue), (blue, green), (green, yellow), (purple, brown), (brown, red), (red, orange), (orange, gray), (yellow, gray)]))
+        
+        self.graphView.frame = self.view.frame
+        self.view.backgroundColor = UIColor.darkTextColor()
+        self.view.addSubview(self.graphView)
+        
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
     }
