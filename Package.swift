cc04ad1913ac672329952a04b07af8d7b77ad94f
Jaden Geller <jaden.geller@gmail.com>
2016-01-23 19:08:07 -0500

    Updated package

--- Package.swift
+++ Package.swift
@@ -4,6 +4,5 @@ import PackageDescription
 let package = Package(
     name: "Logical",
     dependencies: [
-        .Package(url: "https://github.com/JadenGeller/GraphView.git", majorVersion: 1)
     ]
 )
